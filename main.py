671d0bbf43ae6bcf46bd497bac1d9de912fb2e1a
Anirban Basu <anirbanbasu@Anirbans-MacBook-Air.local>
2025-02-07 07:17:22 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 import requests
 
+USERNAME = "anirbanbasu"
+TOKEN = "hhdehd32d9dh93hf332b3b3"
+GRAPH_ID = "graph1"
+
+
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "hhdehd32d9dh93hf332b3b3",
-    "username": "anirbanbasu",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url= pixela_endpoint, json= user_params)
-print(response)
+# response = requests.post(url= pixela_endpoint, json= user_params)
+# print(response.text)
+
+# POST - /v1/users/<username>/graphs
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Book_Reading_Tracker",
+    "unit": "pages",
+    "type": "int",
+    "color": "sora",
+    "timezone": "Asia/Kolkata",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url= graph_endpoint, json= graph_config, headers= headers)
+# print(response.text)
+
+# POST - /v1/users/<username>/graphs/<graphID>
+pixel_creation_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+pixel_creation_config = {
+    "date": "20250207",
+    "quantity": "10"
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_creation_config, headers=headers)
 print(response.text)
+
+
